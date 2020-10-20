cd8f73e1656d98e282ae5fbb03a8d6cd5d4e13e0
alex villarreal <alexvj14@outlook.com>
2020-10-20 00:49:10 -0500

    Creacion de migraciones

--- database/migrations/2020_10_20_044340_crear_tabla_usuario.php
+++ database/migrations/2020_10_20_044340_crear_tabla_usuario.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsuarioTable extends Migration
+class CrearTablaUsuario extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,10 @@ class CreateUsuarioTable extends Migration
     public function up()
     {
         Schema::create('usuario', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->Increments('id');
+            $table->string('usuario',50);
+            $table->string('password',100);
+            $table->string('nombre',50);
             $table->timestamps();
         });
     }
